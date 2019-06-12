0aa2d288b2fbc7b958f608357cfc8cfc35cf2cf9
zxl <14787484930@163,com>
2019-06-12 16:23:54 +0800

    修改

---
+++ src/components/other/AAA.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>辅导内容</h1>
+    <p>{{books}}</p>
+  </div>
+</template>
+
+<script>
+  import {mapGetters} from 'vuex'
+  export default {
+
+    computed:{
+      ...mapGetters([
+        'books'
+      ])
+    },
+
+    //组件加载完后执行的方法
+    created:function(){
+      this.$store.dispatch('getbooks');
+    }
+  }
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
